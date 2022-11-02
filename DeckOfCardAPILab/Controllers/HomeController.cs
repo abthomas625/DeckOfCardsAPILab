b25b58d1ae0c3458e93ac1b70e6835702767b324
Abilena <abthomas625@gmail.com>
2022-11-01 20:19:14 -0500

    pick a card any card

---
+++ DeckOfCardAPILab/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using DeckOfCardAPILab.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace DeckOfCardAPILab.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        DeckDAL api = new DeckDAL();
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        
+        public IActionResult DrawCards()
+        {
+            Deck d = api.GetDeck();
+            NewCard nc = api.Get5Cards(d.deck_id);
+            return View(nc);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ DeckOfCardAPILab/Models/DeckDAL.cs
@@ -0,0 +1,28 @@
+﻿using RestSharp;
+using System;
+
+namespace DeckOfCardAPILab.Models
+{
+    public class DeckDAL
+    {
+        public Deck GetDeck()
+        {            
+            var client = new RestClient($"https://deckofcardsapi.com/api/deck/new/shuffle");
+            var request = new RestRequest();
+            var response = client.GetAsync<Deck>(request);
+            Deck d = response.Result;
+
+            return d;
+        }
+        public NewCard Get5Cards(string deck_id)
+        {
+            var client = new RestClient($"https://deckofcardsapi.com/api/deck/{deck_id}/draw/?count=5");
+            var request = new RestRequest();
+            var response = client.GetAsync<NewCard>(request);
+            NewCard nc = response.Result;
+
+            return nc;
+        }
+
+    }
+}
